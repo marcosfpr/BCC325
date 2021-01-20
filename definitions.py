fdc624b9e1c82e79d2074b7e267a6490223fd4a3
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-01-20 11:18:01 -0300

    Documentation

--- definitions.py
+++ definitions.py
@@ -3,23 +3,40 @@
 import abc
 
 class Environment(abc.ABC):
-    def __init__(self):
-        pass
-
+ 
     @abc.abstractmethod
     def signal(self, action):
+        """
+            Produces the enviroment percepts after action is executed.
+        """
         pass
 
     @abc.abstractmethod
     def initial_percepts(self):
+        """
+            Produces the enviroment initial percepts.
+        """
         pass
 
 class Agent(abc.ABC):
+    """
+        Implements the Agent class
+    """
 
     def __init__(self, env: Environment):
+        """
+            Constructor for the agent class.
+
+            Args:
+                env: a reference to an enviroment
+        """
         self.env = env
 
     @abc.abstractmethod
     def act(self):
+        """
+            Defines the agent action.
+        """
         pass
-   
+
+
