5db0601ef5268ea4cec47d8bf826559c152b2503
marcosfpr <marcos.rezende@aluno.ufop.edu.br>
2021-02-25 08:58:08 -0300

    GAC

--- path_finder_simulation.py
+++ path_finder_simulation.py
@@ -2,12 +2,12 @@ from room import Room
 from path_finder_agents import  IterativeDeepeningAgent, BFSAgent, DFSAgent, AStarAgent, BBAgent, GreedyAgent
 
 # env = Room(room=[[0, 0, 1], [0, 0, 1], [0, 0, 0]], target=[2, 2])
-env = Room(prob=0.2, n=10, plot_on=True)
+env = Room(prob=0.2, n=25, plot_on=True)
 
 # agent = BFSAgent(env)
 # agent = DFSAgent(env)
-# agent = AStarAgent(env)
-agent = GreedyAgent(env)
+agent = AStarAgent(env)
+# agent = GreedyAgent(env)
 
 # agent = BBAgent(env, bound=20)
 # agent = IterativeDeepeningAgent(env)
